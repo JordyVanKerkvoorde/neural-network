06151b12bad746c88c45e9e945ac2aba3ef29c51
Jordy <jordy.vankerkvoorde@student.hogent.be>
2020-11-20 23:43:28 +0100

    Add dotproduct

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # outputs fom neurons in the previous layer
 inputs = [1, 2, 3, 2.5]
 
@@ -10,12 +12,6 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 biases = [2, 3, 0.5]
 
 # calculating output
-layer_outputs = []
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0
-    for n_input, weight in zip (inputs, neuron_weights):
-        neuron_output += n_input * weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
+output = np.dot(weights, inputs) + biases
 
-print(layer_outputs)
+print(output)
